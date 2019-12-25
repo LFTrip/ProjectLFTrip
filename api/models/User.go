1763130e6d29b850e10a584f87f414c7c025e930
muhammad-tounsi <muhammad.tounsi@hotmail.fr>
2019-12-25 19:27:29 +0100

    Feat: User model + package security

---
+++ api/models/User.go
@@ -0,0 +1,278 @@
+package models
+
+import (
+	"errors"
+	"html"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/Muhammad-Tounsi/ProjectLFTrip/api/security"
+
+	"github.com/badoux/checkmail"
+	"github.com/jinzhu/gorm"
+	uuid "github.com/twinj/uuid"
+)
+
+// User Struct
+type User struct {
+	UUID             uuid.UUID `gorm:"type:uuid;unique_index;" json:"uuid"`
+	Firstname        string    `valid:"required,alpha,length(2|255)" json:"firstname"`
+	Lastname         string    `valid:"required,alpha,length(2|255)" json:"lastname"`
+	Email            string    `gorm:"size:100;not null;unique" valid:"email" json:"email"`
+	Password         string    `gorm:"size:100;not null;" json:"password"`
+	Accesslevel      int       `valid:"range(0|1),numeric" json:"access_level"`
+	Dateofbirth      time.Time `gorm:"not null;" validate:"min=18" json:"date_of_birth"`
+	Sexe             string    `gorm:"size:100;not null;" json:"sexe"`
+	City             string    `gorm:"size:250;null;" json:"city"`
+	PhoneNumber      string    `gorm:"size:10;null" json:"phone_number"`
+	DepartureAirport string    `gorm:"size:250;null;" json:"departure_airport"`
+	Description      string    `gorm:"size:250;null;" json:"description"`
+	AvatarPath       string    `gorm:"size:255;null;" json:"avatar_path"`
+	CreatedAt        time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt        time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+// TableName : Gorm related
+func (u *User) TableName() string {
+	return "users"
+}
+
+// BeforeSave : call package security to hash the password
+func (u *User) BeforeSave() error {
+	hashedPassword, err := security.Hash(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	u.UUID = uuid.NewV4()
+	u.CreatedAt = time.Now()
+	return nil
+}
+
+// BeforeUpdate is gorm hook that is triggered on every updated on user struct
+func (u *User) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("UpdatedAt", time.Now())
+	return nil
+}
+
+// Prepare : prepare  statements
+func (u *User) Prepare() {
+	u.UUID = uuid.NewV4()
+	u.Firstname = html.EscapeString(strings.TrimSpace(u.Firstname))
+	u.Lastname = html.EscapeString(strings.TrimSpace(u.Lastname))
+	u.Email = html.EscapeString(strings.TrimSpace(u.Email))
+	u.Sexe = html.EscapeString(strings.TrimSpace(u.Sexe))
+	u.City = html.EscapeString(strings.TrimSpace(u.City))
+	u.PhoneNumber = html.EscapeString(strings.TrimSpace(u.PhoneNumber))
+	u.DepartureAirport = html.EscapeString(strings.TrimSpace(u.DepartureAirport))
+	u.Description = html.EscapeString(strings.TrimSpace(u.Description))
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
+}
+
+// Validate : function to check the data
+func (u *User) Validate(action string) map[string]string {
+	var errorMessages = make(map[string]string)
+	var err error
+
+	switch strings.ToLower(action) {
+	case "update":
+		if u.Email == "" {
+			err = errors.New("Required Email")
+			errorMessages["Required_email"] = err.Error()
+		}
+		if u.Email != "" {
+			if err = checkmail.ValidateFormat(u.Email); err != nil {
+				err = errors.New("Invalid Email")
+				errorMessages["Invalid_email"] = err.Error()
+			}
+		}
+
+	case "login":
+		if u.Password == "" {
+			err = errors.New("Required Password")
+			errorMessages["Required_password"] = err.Error()
+		}
+		if u.Email == "" {
+			err = errors.New("Required Email")
+			errorMessages["Required_email"] = err.Error()
+		}
+		if u.Email != "" {
+			if err = checkmail.ValidateFormat(u.Email); err != nil {
+				err = errors.New("Invalid Email")
+				errorMessages["Invalid_email"] = err.Error()
+			}
+		}
+	case "forgotpassword":
+		if u.Email == "" {
+			err = errors.New("Required Email")
+			errorMessages["Required_email"] = err.Error()
+		}
+		if u.Email != "" {
+			if err = checkmail.ValidateFormat(u.Email); err != nil {
+				err = errors.New("Invalid Email")
+				errorMessages["Invalid_email"] = err.Error()
+			}
+		}
+	default:
+		if u.Firstname == "" {
+			err = errors.New("Required Firstname")
+			errorMessages["Required_firstname"] = err.Error()
+		}
+		if u.Lastname == "" {
+			err = errors.New("Required Lastname")
+			errorMessages["Required_lastname"] = err.Error()
+		}
+		if u.Password == "" {
+			err = errors.New("Required Password")
+			errorMessages["Required_password"] = err.Error()
+		}
+		if u.Password != "" && len(u.Password) < 6 {
+			err = errors.New("Password should be atleast 6 characters")
+			errorMessages["Invalid_password"] = err.Error()
+		}
+		if u.Email == "" {
+			err = errors.New("Required Email")
+			errorMessages["Required_email"] = err.Error()
+
+		}
+		if u.Email != "" {
+			if err = checkmail.ValidateFormat(u.Email); err != nil {
+				err = errors.New("Invalid Email")
+				errorMessages["Invalid_email"] = err.Error()
+			}
+		}
+	}
+	return errorMessages
+}
+
+// AfterFind : values of return
+func (u *User) AfterFind() (err error) {
+	if err != nil {
+		return err
+	}
+	if u.AvatarPath != "" {
+		u.AvatarPath = os.Getenv("DO_SPACES_URL") + u.AvatarPath
+	}
+	//dont return the user password
+	//u.Password = ""
+	return nil
+}
+
+// SaveUser : Method Save User, triggered on every saved on user struct
+func (u *User) SaveUser(db *gorm.DB) (*User, error) {
+	var err error
+	err = db.Debug().Create(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+// FindAllUsers : function to find all users
+func (u *User) FindAllUsers(db *gorm.DB) (*[]User, error) {
+	var err error
+	users := []User{}
+	err = db.Debug().Model(&User{}).Limit(100).Find(&users).Error
+	if err != nil {
+		return &[]User{}, err
+	}
+	return &users, err
+}
+
+// FindUserByID : function to find a user with an ID
+func (u *User) FindUserByID(db *gorm.DB, uid uint64) (*User, error) {
+	var err error
+	err = db.Debug().Model(User{}).Where("uuid = ?", uid).Take(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return &User{}, errors.New("User Not Found")
+	}
+	return u, err
+}
+
+// UpdateAUser : update an user
+func (u *User) UpdateAUser(db *gorm.DB, uid uint64) (*User, error) {
+	// To hash the password
+	err := u.BeforeSave()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = db.Debug().Model(&User{}).Where("uuid = ?", uid).Take(&User{}).UpdateColumns(
+		map[string]interface{}{
+			"password":          u.Password,
+			"firstname":         u.Firstname,
+			"lastname":          u.Lastname,
+			"email":             u.Email,
+			"sexe":              u.Sexe,
+			"city":              u.City,
+			"phone_number":      u.PhoneNumber,
+			"departure_airport": u.DepartureAirport,
+			"description":       u.Description,
+			"update_at":         time.Now(),
+		},
+	)
+	if db.Error != nil {
+		return &User{}, db.Error
+	}
+	// This is the display the updated user
+	err = db.Debug().Model(&User{}).Where("uuid = ?", uid).Take(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+// UpdateAUserAvatar : update an avatar user
+func (u *User) UpdateAUserAvatar(db *gorm.DB, uid uint64) (*User, error) {
+	db = db.Debug().Model(&User{}).Where("uuid = ?", uid).Take(&User{}).UpdateColumns(
+		map[string]interface{}{
+			"avatar_path": u.AvatarPath,
+			"update_at":   time.Now(),
+		},
+	)
+	if db.Error != nil {
+		return &User{}, db.Error
+	}
+	// This is the display the updated user
+	err := db.Debug().Model(&User{}).Where("uuid = ?", uid).Take(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+// DeleteAUser : function to delete the user
+func (u *User) DeleteAUser(db *gorm.DB, uid uint64) (int64, error) {
+	db = db.Debug().Model(&User{}).Where("uuid = ?", uid).Take(&User{}).Delete(&User{})
+
+	if db.Error != nil {
+		return 0, db.Error
+	}
+	return db.RowsAffected, nil
+}
+
+// UpdatePassword : funtion to update the password
+func (u *User) UpdatePassword(db *gorm.DB) error {
+
+	// To hash the password
+	err := u.BeforeSave()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db = db.Debug().Model(&User{}).Where("email = ?", u.Email).Take(&User{}).UpdateColumns(
+		map[string]interface{}{
+			"password":  u.Password,
+			"update_at": time.Now(),
+		},
+	)
+	if db.Error != nil {
+		return db.Error
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/Muhammad-Tounsi/ProjectLFTrip
+
+go 1.13
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a // indirect
+	github.com/aws/aws-sdk-go v1.26.7 // indirect
+	github.com/badoux/checkmail v0.0.0-20181210160741-9661bd69e9ad
+	github.com/bearbin/go-age v0.0.0-20140407072555-316d0c1e7cd1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-contrib/cors v1.3.0 // indirect
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607 // indirect
+	github.com/gin-gonic/gin v1.5.0 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/jinzhu/gorm v1.9.11
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/satori/go.uuid v1.2.0
+	github.com/sendgrid/rest v2.4.1+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.5.0+incompatible // indirect
+	github.com/twinj/uuid v1.0.0
+	golang.org/x/crypto v0.0.0-20191219195013-becbf705a915
+)
